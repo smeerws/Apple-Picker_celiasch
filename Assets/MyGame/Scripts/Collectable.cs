9c8c0d09a43d6efe9f28defaf4c1169248a42ffa
smeerws <hello@smeerws.at>
2019-01-22 04:04:17 +0100

    apples colliders, scene management

--- Assets/MyGame/Scripts/Collectable.cs
+++ Assets/MyGame/Scripts/Collectable.cs
@@ -4,10 +4,51 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-//Methode ResetApple() Reset the apple counter to 0)       
+    private const string TAGPLAYER = "Player";
+    private const string TAGGROUND = "Floor";
+    private const string TAGBADAPPLE = "BadApple";
+
+    public SceneLoader sceneLoader;
+
+    int numberApples;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Methode ResetApple() Reset the apple counter to 0)       
+        numberApples = 0;
     }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger enter" + collision.tag);
+
+        if (collision.tag == TAGGROUND)
+        {
+            if (numberApples >= 0)
+            {
+                numberApples--;
+            }
+
+            Destroy(gameObject);
+        }
+
+        else if (collision.tag == TAGPLAYER && gameObject.tag == "GoodApple")
+        {
+            numberApples++;
+            Destroy(gameObject);
+        }
+
+        else if(collision.tag == TAGPLAYER && gameObject.tag == "BadApple")
+        {
+            sceneLoader.GoToGameOverScene();
+        }
+    }
+    
+
+
 	
 	// Update is called once per frame
 	void Update () {
